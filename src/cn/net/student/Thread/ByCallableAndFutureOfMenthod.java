36a9a7e6e7044660854b45fbd8c6fb77ac13287a
David WEN <33089860+1378811465@users.noreply.github.com>
2018-12-24 14:45:50 +0800

    Update ByCallableAndFutureOfMenthod.java

--- src/cn/net/student/Thread/ByCallableAndFutureOfMenthod.java
+++ src/cn/net/student/Thread/ByCallableAndFutureOfMenthod.java
@@ -7,11 +7,11 @@ public class ByCallableAndFutureOfMenthod implements Callable<Integer> {
 	@Override
 	public Integer call() throws Exception {
 		int i = 0;  
-        for(;i<100;i++)  
-        {  
-            System.out.println("call : "+Thread.currentThread().getName()+" "+i);  
-        }  
-        return i;  
+        	for(;i<100;i++)  
+        	{  
+            		System.out.println("call : "+Thread.currentThread().getName()+" "+i);  
+        	}  
+        	return i;  
 	}
 
 }
